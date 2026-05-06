c768343a5b7caaded08af2b7e0c7864aec862b4a
kvtodev <kvtodev@outlook.com>
2026-05-07 01:04:27 +0800

    refactor tests: improve assertions and add test for config code distribution

--- practice-talkful/before-impl-web-console/tests/refactor_test.py
+++ practice-talkful/before-impl-web-console/tests/refactor_test.py
@@ -6,8 +6,11 @@ class RefactorTest(unittest.TestCase):
     def test_long_function(self):
         main_path = Path("main.py")
         line_count = len(main_path.read_text().splitlines())
-        self.assertLessEqual(line_count, 70, f"main.py has {line_count} lines, tooooooooooo long")
-
+        self.assertLess(line_count, 70, f"main.py has {line_count} lines, tooooooooooo long")
+    def test_move_code(self):
+        main_path = Path("config.py")
+        line_count = len(main_path.read_text().splitlines())
+        self.assertGreater(line_count, 10, f"config.py is nearly empty, why not moving code here ?")
 
 if __name__ == '__main__':
     unittest.main()
